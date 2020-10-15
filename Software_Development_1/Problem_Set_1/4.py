85041c42f26fb1fc293d18680e23e6ff2dcc803f
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2020-10-15 22:21:43 +0100

    Completed 4

--- Software_Development_1/Problem_Set_1/4.py
+++ Software_Development_1/Problem_Set_1/4.py
@@ -1,5 +1,24 @@
+# Program entry point
 def main():
-    pass
+
+    # Print each column number
+    for x in range(10):
+        if x == 0:
+            print("{:>6}".format(x + 1), end=" ")
+        else:
+            print("{:>2}".format(x + 1), end=" ")
+
+    print("")
+
+    for x in range(10):
+        # Print row number
+        print("{:>2}".format(x + 1), end="  ")
+
+        # For each row print column numbers
+        for y in range(10):
+            print("{:>2}".format((x+1) * (y+1)), end=" ")
+
+        print("")
 
 if __name__ == "__main__":
     main()
