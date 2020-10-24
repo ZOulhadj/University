9a59bc8eaa43830b77870ce263726ccd7329aec4
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2020-10-24 18:27:17 +0100

    Cleaned up number 5 by asking for monthly savings directly

--- Software_Development_1/Problem_Set_1/5.py
+++ Software_Development_1/Problem_Set_1/5.py
@@ -16,15 +16,20 @@ def main():
         try:
             house_cost = float(input("Enter the cost of your future home: "))
             annual_salary = float(input("Enter you annual salary: "))
-            savings_reserve = float(input("Enter the portion of salary to be saved: "))
+            savings_percentage = float(input("Enter a percentage of your monthly salary to be saved (0% to 10%): "))
+
+            # Ensure savings percentage input is within bounds
+            if savings_percentage < 0 or savings_percentage > 10:
+                print("ERROR: Please enter a savings percentage of 0% to 10% of your monthly salary")
+                raise ValueError
 
             # Throw an error if savings is greater than annual salary
-            if savings_reserve > annual_salary:
+            if savings_percentage > annual_salary:
                 print("ERROR: Annual savings is greater than annual salary")
                 continue
 
         except ValueError:
-            print("ERROR: Please enter a numerical value.")
+            print("ERROR: Incorrect input")
             continue
 
         else:
@@ -37,25 +42,11 @@ def main():
     # The amount of down payment based on house price
     down_payment = find_percentage(25, house_cost)
 
-    # This sets how much will be saved per year in savings
-    annual_savings = annual_salary - savings_reserve
-
     # Monthly salary
     monthly_salary = annual_salary / 12
 
     # Calculate savings amount per month
-    monthly_savings = annual_savings / 12
-
-    # Calculate savings percentage based on monthly salary
-    savings_percentage = (monthly_savings / monthly_salary) * 100.0
-
-    # If savings amount is greater than 10% of their monthly salary then set a 10% limit on savings
-    savings_before_limit = None
-    if savings_percentage > 10.0:
-        savings_before_limit = monthly_savings
-
-        # Set monthly savings to 10% of their monthly salary
-        monthly_savings = find_percentage(10, monthly_salary)
+    monthly_savings = find_percentage(savings_percentage, monthly_salary)
 
     # Calculate return of investments based on 0.04% of savings per month
     monthly_interest = find_percentage(0.04, monthly_savings)
@@ -66,8 +57,6 @@ def main():
     # Calculate how long in months it will take to afford the down payment
     duration_months = down_payment / monthly_savings
     duration_days = ceil((duration_months % 1) * 30.417)
-
-
 
     # Output information
     print("It will take ", floor(duration_months), " month(s) and ", duration_days, " days to save up for the down payment")
@@ -83,10 +72,6 @@ def main():
     print("")
 
     print("Monthly savings (before 0.04% added interest): " + "£{:,.2f}".format(monthly_savings - monthly_interest))
-
-    # Only display monthly savings before if that applies
-    if savings_before_limit:
-        print("Monthly savings (before 10% savings limit): " + "£{:,.2f}".format(savings_before_limit))
 
 
 if __name__ == "__main__":
