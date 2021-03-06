fe7431a5949b07f4243c9e84bbc44f63bf79f657
ZOulhadj <zakariyaoulhadj01@gmail.com>
2021-03-06 01:13:29 +0100

    Got input and output file working

--- software_development_2/case_converter/main.c
+++ software_development_2/case_converter/main.c
@@ -30,12 +30,19 @@ enum output
 
 struct arguments
 {
+    // conversion info
     enum cases case_conversion;
     bool count;
 
+    // buffer info
+    char buffer[500];
+    int word_count;
+
+    // input info
     enum input input_method;
     const char *input_file;
 
+    // output info
     enum output output_method;
     const char *output_file;
 };
@@ -119,6 +126,7 @@ void display_syntax()
     printf("\n");
 }
 
+
 bool is_alphabet(int code)
 {
     // check if given code is within the alphabet
@@ -127,7 +135,7 @@ bool is_alphabet(int code)
 
 void tolower_case(char *c)
 {
-    // obtain the chracters ascii code
+    // obtain the characters ascii code
     int ascii_code = (int)*c;
 
     // if character is not within the alphabet ignore conversion
@@ -140,7 +148,7 @@ void tolower_case(char *c)
 
 void toupper_case(char *c)
 {
-    // obtain the chracters ascii code
+    // obtain the characters ascii code
     int ascii_code = (int)*c;
 
     // if character is not within the alphabet ignore conversion
@@ -149,6 +157,122 @@ void toupper_case(char *c)
 
     // convert to lower case
     *c = char_upper_case(ascii_code);
+}
+
+
+bool read_data(struct arguments *args)
+{
+    if (args->input_method == FILE_INPUT)
+    {
+        // open file specified by the user
+        FILE *file = fopen(args->input_file, "r");
+        if (!file)
+        {
+            printf("%s\n", "Error: Failed to read input file!");
+            return false;
+        }
+
+        // read contents of file into buffer array
+        int c, i = 0;
+        while ((c = fgetc(file)) != EOF)
+        {
+            args->buffer[i] = (char)c;
+            ++i;
+        }
+
+
+        if (args->case_conversion == UPPER_CASE)
+        {
+            for (int i = 0; args->buffer[i] != '\0'; ++i)
+            {
+                toupper_case(&args->buffer[i]);
+
+                // increment word count when a space is found
+                if (args->buffer[i] == ' ')
+                    ++args->word_count;
+            }
+
+        }
+        else if (args->case_conversion == LOWER_CASE)
+        {
+            for (int i = 0; args->buffer[i] != '\0'; ++i)
+            {
+                tolower_case(&args->buffer[i]);
+
+                // increment word count when a space is found
+                if (args->buffer[i] == ' ')
+                    ++args->word_count;
+            }
+
+        }
+
+    }
+    else if (args->input_method == USER_INPUT)
+    {
+        printf("%s", "Enter text: ");
+        fgets(args->buffer, sizeof(args->buffer), stdin);
+
+
+        if (args->case_conversion == UPPER_CASE)
+        {
+            for (int i = 0; args->buffer[i] != '\0'; ++i)
+            {
+                toupper_case(&args->buffer[i]);
+
+                // increment word count when a space is found
+                if (args->buffer[i] == ' ')
+                    ++args->word_count;
+            }
+
+        }
+        else if (args->case_conversion == LOWER_CASE)
+        {
+            for (int i = 0; args->buffer[i] != '\0'; ++i)
+            {
+                tolower_case(&args->buffer[i]);
+
+                // increment word count when a space is found
+                if (args->buffer[i] == ' ')
+                    ++args->word_count;
+            }
+
+        }
+
+    }
+
+    return true;
+}
+
+bool output_data(struct arguments *args)
+{
+
+    // output the data
+    if (args->output_method == FILE_OUTPUT)
+    {
+        FILE *file = fopen(args->output_file, "w");
+
+        if (!file)
+        {
+            printf("%s\n", "Error: Failed to write output file!");
+            return false;
+        }
+
+        // write buffer into file
+        for (int i = 0; args->buffer[i] != '\0'; ++i)
+        {
+            fprintf(file, "%c", args->buffer[i]);
+        }
+    }
+    else if (args->output_method == USER_OUTPUT)
+    {
+        // print the converted text onto the console
+        for (int i = 0; args->buffer[i] != '\0'; ++i)
+        {
+            printf("%c", args->buffer[i]);
+        }
+    }
+
+    return true;
 }
 
 
@@ -165,9 +289,7 @@ int main(int argc, char **argv)
     struct arguments args = {};
     args.case_conversion = DEFAULT_CASE;
     args.count = false;
-    char buffer[500];
 
-    int word_count = 0;
 
     // parse arguments
     bool parse = parse_arguments(argc, argv, &args);
@@ -179,63 +301,23 @@ int main(int argc, char **argv)
         return 0;
     }
 
-
-    if (args.input_method == FILE_INPUT)
+    // read data from the user (file or stdin)
+    bool read = read_data(&args);
+    if (!read)
     {
-        FILE *file = fopen(args.input_file, "r");
-        if (file)
-        {
-
-        }
-    }
-    else if (args.input_method == USER_INPUT)
-    {
-
-        printf("%s", "Enter text: ");
-        fgets(buffer, sizeof(buffer), stdin);
-
-
-        if (args.case_conversion == UPPER_CASE)
-        {
-            for (int i = 0; buffer[i] != '\0'; ++i)
-            {
-                toupper_case(&buffer[i]);
-
-                // increment word count when a space is found
-                if (buffer[i] == ' ')
-                    ++word_count;
-            }
-
-        }
-        else if (args.case_conversion == LOWER_CASE)
-        {
-            for (int i = 0; buffer[i] != '\0'; ++i)
-            {
-                tolower_case(&buffer[i]);
-
-                // increment word count when a space is found
-                if (buffer[i] == ' ')
-                    ++word_count;
-            }
-
-        }
-
+        printf("%s\n", "Error: Failed to read input data!");
+        return 0;
     }
 
 
-    // output the data
-    if (args.output_method == FILE_OUTPUT)
+    // Note: find out if outputting data can really return false
+    bool output = output_data(&args);
+    if (!output)
     {
+        printf("%s\n", "Error: Failed to output data!");
+        return 0;
+    }
 
-    }
-    else if (args.output_method == USER_OUTPUT)
-    {
-        // print the converted text onto the console
-        for (int i = 0; buffer[i] != '\0'; ++i)
-        {
-            printf("%c", buffer[i]);
-        }
-    }
 
     return 0;
 }
